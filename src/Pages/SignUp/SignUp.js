c443dd7829f1f5f40fbf7cd31a1b20919c615d15
ashimbhowmik <ashimbhowmik239@gmail.com>
2022-12-02 20:14:39 +0600

    [sign up page]

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -29,6 +29,9 @@ const SignUp = () => {
   return (
     <div className="hero w-full my-20">
       <div>
+        <div>
+          <img src="" alt="" />
+        </div>
         <div className="card w-[800px] max-w-sm shadow-2xl bg-base-100 py-10">
           <h1 className="text-center text-5xl font-bold">Sign Up now!</h1>
           <form onSubmit={handleSubmit(handleSignUp)} className="card-body">
